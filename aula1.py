81f9756de7bccd35777bac9e387694750590c85e
Diego <diegosccpinto@gmail.com>
2023-08-14 13:10:08 -0300

    Initial commit

---
+++ aula1.py
@@ -0,0 +1,11 @@
+"""
+    Fatiamento de Strings
+    0123456789.............33
+    - Olá mundo
+    987654321
+    Fatiamento [inicio:fim:passo] [::]
+    Obs. : a função len() conta a quantidade de caracteres de uma string
+"""
+
+variavel = "Olá Mundo!"
+print(variavel[0:4]) # imprime Olá
